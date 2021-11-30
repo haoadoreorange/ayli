f5fa6637d598a850f7269e94937254c75378847d
haoadoresorange <github@comtam.dev>
2021-11-29 20:45:28 -0500

    Improve ts-node template
    - Fix bug: add sourceRoot to gulpfile
    - Add yarn scripts for upgrade dependencies
    - Add options to tsconfig.build
    - Add deps description
    - Add travis-ci
    
    Signed-off-by: haoadoresorange <github@comtam.dev>

--- templates/ts-node/src/index.ts
+++ templates/ts-node/src/index.ts
@@ -3,3 +3,16 @@ process.on(`unhandledRejection`, (reason, promise) => {
     process.exit(1);
 });
 import "lib/polyfill";
+
+if (process.argv[2] === `--upgrade`) {
+    void import(`fs`).then((fs) => {
+        const package_json_path = `package.json`;
+        let ext = `bak`;
+        let i = 0;
+        while (fs.existsSync(`${package_json_path}.${ext}`)) {
+            ext = `bak.${i}`;
+            i++;
+        }
+        fs.copyFileSync(package_json_path, `${package_json_path}.${ext}`);
+    });
+} else console.log(`To upgrade semver or latest dependencies, run 'yarn semver' or 'yarn latest'`);
